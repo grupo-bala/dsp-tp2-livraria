d7b26d81790fc2b8b7f5387584715f062cfd0fcd
Hugo Lobo <hugorplobo@gmail.com>
2025-01-22 20:58:18 -0300

    feat: add edition route

---
+++ src/database/edition.py
@@ -0,0 +1,29 @@
+from src.models import Edition
+from .infra import engine
+from sqlmodel import Session, select
+from typing import Optional
+
+def filter_editions(
+    page: int,
+    size: int,
+    isbn: Optional[str] = None,
+    price: Optional[float] = None,
+    publisher: Optional[str] = None,
+    language: Optional[str] = None,
+    publication_year: Optional[int] = None,
+    stock: Optional[int] = None,
+    book_id: Optional[int] = None
+):
+    with Session(engine) as session:
+        statement = select(Edition) \
+            .where(isbn == None or Edition.isbn == isbn) \
+            .where(price == None or Edition.price == price) \
+            .where(publisher == None or publisher.lower() in Edition.publisher.lower()) \
+            .where(language == None or language.lower() in Edition.language.lower()) \
+            .where(publication_year == None or Edition.publication_year == publication_year) \
+            .where(stock == None or Edition.stock == stock) \
+            .where(book_id == None or Edition.book_id == book_id)
+        
+        statement = statement.offset((page - 1) * size).limit(size)
+        
+        return session.exec(statement).all()
--- src/routes/edition.py
+++ src/routes/edition.py
@@ -1,13 +1,33 @@
 from ..models import Edition
 from ..database.infra import get_db
+from ..database.edition import filter_editions
 from fastapi_crudrouter import SQLAlchemyCRUDRouter
+from typing import Annotated, Optional
+from fastapi import Query
 
 edition_router = SQLAlchemyCRUDRouter(
     schema=Edition,
     db_model=Edition,
+    create_schema=Edition,
     db=get_db
 )
 
 @edition_router.get("")
-def get_edtions():
-    return ""
+def get_edtions(
+    page: int,
+    size: int,
+    isbn: Annotated[str | None, Query(min_length=1)] = None,
+    price: Optional[float] = None,
+    publisher: Annotated[str | None, Query(min_length=1)] = None,
+    language: Annotated[str | None, Query(min_length=1)] = None,
+    publication_year: Optional[int] = None,
+    stock: Optional[int] = None,
+    book_id: Optional[int] = None,
+):
+    editions = filter_editions(page, size, isbn, price, publisher, language, publication_year, stock, book_id)
+
+    return {
+        "editions": editions,
+        "page": page,
+        "size": len(editions)
+    }
